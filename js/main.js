b297b20d9c3f512a5ab80d1d5cee0f12e13948a2
felipe-souza17 <felipesantos.rsouza@gmail.com>
2021-09-17 01:06:00 -0300

    handleErrors

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@
 let btnElement = document.getElementById("btnElement")
 
 const showData = result => {
+    document.querySelector("#user-not-found").style.display = "none"
+    document.querySelector(".content").style.display = "flex"
     const months = [
         "Jan", "Feb", "Mar", "Apr",
         "May", "Jun", "Jul", "Aug",
@@ -22,15 +24,38 @@ const showData = result => {
     for(let campo in result) {
         if(document.querySelector("#"+campo)) {
             document.querySelector("#"+campo).innerHTML = result[campo]
-            console.log(campo + "=" + result[campo])
+            // console.log(campo + "=" + result[campo])
         }
     }
     document.querySelector("#avatar_url").src = `${result['avatar_url']}`
     document.querySelector("#created_at").innerHTML = `Joined ${day} ${month} ${year}`
 
     const adjustEmptyValues = () => {
-        if(!result['bio']) {
-            document.querySelector("#bio").innerHTML = "This profile has no bio"
+        const arr = [
+            "company", "twitter_username",
+            "location", "bio"
+        ]
+        for(let campo in arr) {
+            if(!result[arr[campo]]) {
+                document.querySelector("#"+arr[campo]).innerHTML = "Not Available"
+                document.querySelector("#"+arr[campo]).style.color = 'var(--dark-gray)'
+                if(!result['bio']) {
+                    document.querySelector("#bio").innerHTML = "This profile has no bio"
+                }
+            } else {
+                document.querySelector("#"+arr[campo]).style.color = 'var(--gray)'
+            }
+            if(result['blog'].length == 0) {
+                document.querySelector('#blog').innerHTML = "Not Available"
+                document.querySelector('#blog').href = ""
+                document.querySelector("#blog").style.color = 'var(--dark-gray)'
+            }
+            else {
+                document.querySelector('#blog').style.color = 'var(--bright-blue)'
+                document.querySelector('#blog').href = result['blog']
+            }
+            /*console.log("========")
+            console.log(arr[campo])*/
         }
     }
     adjustEmptyValues()
@@ -43,13 +68,20 @@ btnElement.addEventListener("click", e => {
         mode: 'cors',
         cache: 'default'
     }
+    const handleError = response => {
+        if(response.status == 404) throw Error(response.statusText)
+        return response
+    }
 
     fetch(`https://api.github.com/users/${user}`, options)
-        .then((response) => { response.json()
-            .then( data => showData(data))
+    .then(handleError)
+        .then(response => { 
+            response.json()
+                .then( data => showData(data), console.log(response))
         })
-        .catch( e => {
-            console.log('Deu Erro:'+ e.message)
-            console.log(user)
+        .catch( error => {
+            document.querySelector("#user-not-found").style.display = "block"
+            document.querySelector(".content").style.display = "none"
+            console.log(error)
         })
 })
